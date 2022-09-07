da842ab66f42949d8c14ccf8f96945d477e7b505
John Hozin <simple_protector@mail.ru>
2022-09-07 18:02:35 +0300

    make3

---
+++ Sem3Task20/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу, которая принимает на вход
+// координаты двух точек и находит расстояние между
+// ними в 2D пространстве.
+
+// чтение данных из консоли
+int ReadData(string line)
+{
+    // Выводим сообщение
+    Console.WriteLine(line);
+    // Считываем число
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    // Возвращаем значение
+    return number;
+}
+
+// Вычисляем расстояние между двумя точками
+double Calculate(int x1, int y1, int x2, int y2)
+{
+    return Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+}
+
+void PrintResult(string line)
+{
+    Console.WriteLine(line);
+}
+
+// Соберем данные
+int x1 = ReadData("Введите x1: ");
+int x2 = ReadData("Введите x2: ");
+int y1 = ReadData("Введите y1: ");
+int y2 = ReadData("Введите y2: ");
+
+// Выводим результат
+PrintResult(Math.Round(Calculate(x1, x2, y1, y2), 2).ToString());
---
+++ Sem3Task22/Program.cs
@@ -0,0 +1,71 @@
+﻿// Напишите программу, которая принимает на вход
+// число (N) и выдаёт таблицу квадратов чисел от 1 до
+// N.
+
+// чтение данных из консоли
+int ReadData(string line)
+{
+    // Выводим сообщение
+    Console.WriteLine(line);
+    // Считываем число
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    // Возвращаем значение
+    return number;
+}
+
+// 1 2 3 4 5 6 
+// 1 4 9 16 25 36
+// string LineNumbers(int numberN)
+// {
+//     int i = 1;
+//     string outLine = string.Empty;
+//     while (i < numberN)
+//     {
+//         outLine = outLine + i + "\t";
+//         i++;
+//     }
+//     outLine = outLine + numberN;
+//     return outLine;
+// }
+
+// создаёт строчку от 1 до N(+i) и вторую от 1 до N^2(+i^2)
+string LineNumbersPow(int numberN, int pow)
+{
+    int i = 1;
+    string outLine = string.Empty;
+    while (i < numberN)
+    {
+        outLine = outLine + Math.Pow(i,pow) + "\t";
+        i++;
+    }
+    outLine = outLine + Math.Pow(numberN,pow);
+    return outLine;
+}
+
+// string LineSqer(int numberN)
+// {
+//     int i = 1;
+//     string outLine = string.Empty;
+//     while (i < numberN)
+//     {
+//         outLine = outLine + i*i + "\t";
+//         i++;
+//     }
+//     outLine = outLine + numberN*numberN;
+//     return outLine;
+// }
+
+void PrintResult(string line)
+{
+    Console.WriteLine(line);
+}
+
+int num = ReadData("Введите число N: ");
+// string lineTop = LineNumbers(num);
+// string lineDown = LineSqer(num);
+
+// PrintResult(lineTop);
+// PrintResult(lineDown);
+
+PrintResult(LineNumbersPow(num,1));
+PrintResult(LineNumbersPow(num,2));
