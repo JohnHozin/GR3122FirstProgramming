514521979212746e2386540b2ea964a3b4985383
JohnHozin <simple_protector@mail.ru>
2022-09-16 22:20:25 +0300

    make class work 6

---
+++ Sem6Task45/Program.cs
@@ -0,0 +1,59 @@
+﻿// ---------------------------------------------------------------------
+//                                 Задача 45
+// Напишите программу, которая будет создавать
+// копию заданного одномерного массива с помощью
+// поэлементного копирования.
+// ---------------------------------------------------------------------
+
+
+// Печать результата
+void PrintResult(string line)
+{
+    Console.WriteLine(line);
+}
+
+// Заполняем массив
+int[] GenArray(int arrLength, int start, int stop)
+{
+    int[] array = new int[arrLength];
+    Random ren = new Random();
+
+    for (int i = 0; i < arrLength; i++)
+    {
+        array[i] = ren.Next(start, stop + 1);
+    }
+    return array;
+}
+
+// печатаем массив
+void PrintArray(int[] arr)
+{
+    Console.Write("[" + arr[0] + ", ");
+    for (int i = 1; i < arr.Length - 1; i++)
+    {
+        Console.Write(arr[i] + ", ");
+    }
+    Console.Write(arr[arr.Length - 1] + "]");
+    Console.WriteLine();
+}
+
+
+int[] CopyArr(int[] arr)
+{
+    int[] outArr = new int[arr.Length];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        outArr[i] = arr[i];
+    }
+    return outArr;
+}
+
+int[] arr = GenArray(20, 1, 100);
+PrintArray(arr);
+int[] copyArr = CopyArr(arr);
+PrintArray(copyArr);
+
+
+PrintArray((int[])arr.Clone());
+
+
