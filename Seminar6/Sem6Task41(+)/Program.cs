4332faf582b7d1fa2b12564e14fc3e55e604379c
JohnHozin <simple_protector@mail.ru>
2022-09-18 00:58:06 +0300

    make Home Work 6

---
+++ Seminar6/Sem6Task41(+)/Program.cs
@@ -0,0 +1,82 @@
+﻿// ---------------------------------------------------------------------
+//                                 Задача 41(+)
+// * Пользователь вводит число нажатий, затем программа следит за нажатиями и
+// выдает сколько чисел больше 0 было введено.
+// ---------------------------------------------------------------------
+
+using System.Text.RegularExpressions;
+
+// Чтение данных из консоли
+int ReadData(string line)
+{
+    // Выводим сообщение
+    Console.Write(line);
+    // Считываем число
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    // Возвращаем значение
+    return number;
+}
+
+// // Печать результата
+// void PrintResult(string prefix, string data)
+// {
+//     Console.WriteLine(prefix + data);
+// }
+
+// // Считаем положительные числа
+// int Count(int m)
+// {
+//     // int res = 0;
+//     // for (int i = 0; i < m; i++)
+//     // {
+//     //     int key = 0;
+//     //     //int num = ReadData("Введите число: ");
+//     //     key = Console.ReadKey();
+//     //     if (key > 0) res++;
+//     // }
+
+//     // return res;
+
+// }
+
+// PrintResult("Пользователь ввёл чисел больше ноля: ", Count(ReadData("Введите количество чисел: ")).ToString());
+
+// //*
+// // key = Console.ReadKey();
+// // key = ConsoleKey.D0;
+// // key = ConsoleKey.NumPad0;
+
+
+
+//  Метод Анатолия
+// Считывание клавиши.
+string ReadRow(int count)
+{
+    string row = string.Empty;
+    for (int i = 0; i < count; i++)
+    {
+        row += Console.ReadKey(true).KeyChar.ToString();
+    }
+    return row;
+}
+
+// Печать строки.
+void PrintData(string data, string pref = "", string post = "", string end = "\n")
+{
+    Console.Write($"{pref}{data}{post}{end}");
+}
+
+// Список положительных чисел.
+MatchCollection GetNumbers(string row)
+{
+    Regex regex = new Regex(@"(?<!-[0-9]*)0*[1-9]+[0-9]*");
+    MatchCollection matches = regex.Matches(row);
+    return matches;
+}
+
+string row = ReadRow(ReadData("Введите число "));
+MatchCollection matches = GetNumbers(row);
+
+PrintData(matches.Count.ToString(), "Вы ввели ", " положительных чисел.");
+PrintData(row, "Исходная строка: ");
+PrintData(string.Join(", ", matches), "Список положительных чисел: [", "]");
---
+++ Seminar6/Sem6Task43(+)/Program.cs
@@ -0,0 +1,75 @@
+﻿// ---------------------------------------------------------------------
+//                                 Задача 43(+)
+// * Найдите площадь треугольника образованного пересечением 3 прямых
+// ---------------------------------------------------------------------
+
+// Чтение данных из консоли
+double ReadData(string line)
+{
+    // Выводим сообщение
+    Console.Write(line);
+    // Считываем число
+    double number = double.Parse(Console.ReadLine() ?? "0");
+    // Возвращаем значение
+    return number;
+}
+
+// Печать результата
+void PrintResult(string prefix, string data)
+{
+    Console.WriteLine(prefix + data);
+}
+
+// Поиск точки пересечения двух прямых
+double[] PointFind(double k1, double b1, double k2, double b2)
+{
+    double[] outtArr = new double[2];
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+    outtArr[0] = x;
+    outtArr[1] = y;
+    return outtArr;
+}
+
+// Нахождение длины отрезка по двум точкам
+double Segment(double[] pointA, double[] pointB)
+{
+    return Math.Sqrt(Math.Pow(pointA[0] - pointB[0], 2) + Math.Pow(pointA[1] - pointB[1], 2));
+}
+
+// Нахождение площади треугольника
+double SquareTriangle(double AB, double BC, double CA)
+{
+    double p = (AB + BC + CA) / 2;
+    return Math.Round(Math.Sqrt(p * (p - AB) * (p - BC) * (p - CA)), 2);
+}
+
+// Нахождение площади треугольника по кординатам вершин
+double SquareTriangleWithPoint(double[] pointA, double[] pointB, double[] pointC)
+{
+    return Math.Abs((pointB[0] - pointA[0]) * (pointC[1] - pointA[1]) - (pointC[0] - pointA[0]) * (pointB[1] - pointA[1])) / 2;
+}
+
+double k1 = ReadData("Введите k1: ");
+double b1 = ReadData("Введите b1: ");
+double k2 = ReadData("Введите k2: ");
+double b2 = ReadData("Введите b2: ");
+double k3 = ReadData("Введите k3: ");
+double b3 = ReadData("Введите b3: ");
+
+double[] pointA = PointFind(k1, b1, k2, b2);
+double[] pointB = PointFind(k1, b1, k3, b3);
+double[] pointC = PointFind(k3, b3, k2, b2);
+
+double AB = Segment(pointA, pointB);
+double BC = Segment(pointC, pointB);
+double CA = Segment(pointA, pointC);
+// PrintResult("AB ", AB.ToString());
+// PrintResult("BC ", BC.ToString());
+// PrintResult("CA ", CA.ToString());
+
+double square = SquareTriangle(AB, BC, CA);
+double squarePoint = SquareTriangleWithPoint(pointA, pointB, pointC);
+
+PrintResult("Площадь треугольника равна: ", square.ToString());
+PrintResult("Площадь треугольника равна (Второй вариант): ", squarePoint.ToString());
